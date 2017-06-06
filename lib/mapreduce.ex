2bebf566d2b45123a2e044848f67e1fc5c9a8f68
Rudi Chen <rudichen@gmail.com>
2017-06-06 18:20:31 -0400

    Initial commit

---
+++ lib/mapreduce.ex
@@ -0,0 +1,18 @@
+defmodule Mapreduce do
+  @moduledoc """
+  Documentation for Mapreduce.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mapreduce.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mapreduce_test.exs
@@ -0,0 +1,8 @@
+defmodule MapreduceTest do
+  use ExUnit.Case
+  doctest Mapreduce
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
